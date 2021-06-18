e7f1378d116d7f03a1d6200ebf5b1a85f3bc42f8
Javier Arce <4933+javierarce@users.noreply.github.com>
2021-06-18 17:33:35 +0200

    Adds pagination

--- public/js/Preview.js
+++ public/js/Preview.js
@@ -2,6 +2,8 @@ class Preview {
   constructor (entries) {
     this.className = this.constructor.name
     this.entries = entries
+    this.currentPage = 0
+    this.pages = []
     this.render()
   }
 
@@ -13,7 +15,7 @@ class Preview {
     this.$element.classList.remove('is-visible')
   }
 
-  renderEntry (entry) {
+  renderEntry (entry, index) {
     let $element = createElement({ className: 'Entry' })
 
     let author = entry.author ? toTitleCase(entry.author) : 'Unknown'
@@ -25,21 +27,60 @@ class Preview {
     let $content = createElement({ className: 'Entry__content', html: entry.content })
     let $date = createElement({ className: 'Entry__date', html: date })
 
+    $element.id = index
     $element.appendChild($date)
     $element.appendChild($title)
     $element.appendChild($summary)
     $element.appendChild($content)
 
-    this.$element.appendChild($element)
+    this.$entries.appendChild($element)
+    this.pages.push($element)
   }
 
   renderEntries () {
+    this.$entries = createElement({ className: 'Entries' })
+    this.$element.appendChild(this.$entries)
+
     this.entries.forEach(this.renderEntry.bind(this))
+  }
+
+  renderPagination () {
+    this.$pagination = createElement({ className: 'Pagination' })
+
+    this.pages.forEach((page, index) => {
+      let $item = createElement({ type: 'button', className: 'Pagination__item' })
+      $item.id = index
+
+      if (!index) {
+        $item.classList.add('is-selected')
+        this.currentPaginationItem = $item
+      }
+
+      $item.onclick = (event) => {
+        let $element = event.target
+        this.pages[this.currentPage].classList.remove('is-visible')
+        this.currentPage = $element.id
+        this.pages[this.currentPage].classList.add('is-visible')
+        this.currentPaginationItem.classList.remove('is-selected')
+
+        $element.classList.add('is-selected')
+        this.currentPaginationItem = $element
+      }
+
+      $item.innerHTML = index
+      this.$pagination.appendChild($item)
+    })
+
+    this.$element.appendChild(this.$pagination)
   }
 
   render () {
     this.$element = createElement({ className: this.className })
+
     this.renderEntries()
+    this.renderPagination()
+
+    this.pages[0].classList.add('is-visible')
 
     setTimeout(() => {
       this.$element.classList.add('is-visible')
